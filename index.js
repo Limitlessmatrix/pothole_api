69730529e3f46ee720deb3503a995a4f1ead3007
Limitlessmatrix <limitbreakerofchains@protonmail.com>
2023-10-03 21:56:16 -0600

    start handling file uploads

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const formidable = require('formidable').default
 const app = express()
+const urlDecodingMiddleware = express.urlencoded()
+app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+let message = "Getting swifty"
+app.get('/secret', function (req, res) {
+  res.send(message)
+})
+app.post('/submit', urlDecodingMiddleware, function (req, res) {
+  console.log("what is the request body?", req)
+  const form = formidable({});
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json({ fields, files });
+  });
 })
 
 app.listen(3000)
