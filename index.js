7e2e6a89e0fdb710986663b7c1521fa6b8c78a2d
Limitlessmatrix <limitbreakerofchains@protonmail.com>
2023-10-10 21:08:39 -0600

    add express logging middle ware that rejects requests to admin

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const formidable = require('formidable').default
 const app = express()
-const urlDecodingMiddleware = express.urlencoded()
+
+const loggingMiddleWare = (request, response, next) => {
+  console.log(`method: ${request.method}, path: ${request.url}`)
+  if (request.method === "POST" && request.url === "/admin") {
+    response.json({ error: "NO YOU DONT" })
+  } else { 
+    next() 
+  }
+}
+
+app.use(loggingMiddleWare)
 app.use(express.static('public'))
 
 let message = "Getting swifty"
 app.get('/secret', function (req, res) {
   res.send(message)
 })
-app.post('/submit', urlDecodingMiddleware, function (req, res) {
+app.post('/submit', function (req, res) {
   console.log("what is the request body?", req)
   const form = formidable({});
 
